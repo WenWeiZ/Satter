f2b97d13472b2de3ec953648b2f1e9888eb45454
YEZ <1800012788@pku.edu.cn>
2021-09-08 10:27:28 +0800

    cm

---
+++ CM.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import numpy.matlib
+import numpy.linalg
+from scipy import special
+from matplotlib import pyplot as plt
+
+def CM(point, kwave):
+	pass
+
+
+if __name__ == '__main__':
+	y0 = special.j0(0)
+	print(y0)
